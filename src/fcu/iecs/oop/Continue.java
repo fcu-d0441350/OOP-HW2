968e5606ed7c9f1ee5d4bbf751f1d046a9e17a6e
user <user@FC03>
2016-10-14 09:56:13 +0800

    Breah,Continue,修改String

---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,15 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 5; i ++) {
+			if(i!=1 && i!=3)
+				continue;
+			System.out.println(i);
+			}
+
+	}
+
+}
--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -5,15 +5,19 @@ public class StringComparator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String str1,str2;
-		System.out.println("Enter a string 1");
-		str1=scanner.next();
-		System.out.println("Enter a string 2");
-		str2=scanner.next();
-		if(str1.equalsIgnoreCase(str2)) {
-			System.out.println("The two strings are the same");
-		}
-		else {
-			System.out.println("The two strings are not the same");
+		while(true) {
+			System.out.println("Enter a string 1");
+			str1=scanner.next();
+			System.out.println("Enter a string 2");
+			str2=scanner.next();
+			if(str1.equalsIgnoreCase(str2)) {
+				System.out.println("The two strings are the same");
+				break;
+			}
+			else {
+				System.out.println("The two strings are not the same");
+				continue;
+			}
 		}
 		scanner.close();
 
